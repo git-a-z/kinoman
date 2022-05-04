d3f68ba38f4dcad305844856386d17b2b1967294
a_z <azdvd@mail.ru>
2022-05-04 19:05:08 +0300

    az_05.04

---
+++ kinoman/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected string $redirectTo = RouteServiceProvider::CATALOG;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    protected function redirectTo(): string|UrlGenerator|Application
+    {
+        return url($this->redirectTo);
+    }
+
+    public function logout(Request $request): Redirector|Application|RedirectResponse
+    {
+        Auth::logout();
+        return redirect()->back();
+    }
+}
--- kinoman/routes/web.php
+++ kinoman/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,7 +28,29 @@ Route::get('/catalog', [
     'as' => 'catalog'
 ]);
 
-Route::get('/film/{id}', [
+Route::get('/collections', [
+    'uses' => 'App\Http\Controllers\CollectionController@index',
+    'as' => 'collections'
+]);
+
+//Route::get('/film/{id}', [
+//    'uses' => 'App\Http\Controllers\CatalogController@film',
+//    'as' => 'film'
+//]);
+
+Route::get('/film/{filmInfoView}', [
     'uses' => 'App\Http\Controllers\CatalogController@film',
     'as' => 'film'
 ]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home');
+
+Route::get('/profile', [
+    'uses' => 'App\Http\Controllers\ProfileController@index',
+    'as' => 'profile'
+]);
+
+
